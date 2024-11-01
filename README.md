2e14ba0c35d97048738aba90bb8c999a3636fb20
Tubaina <142503145+ServantofTheLordUwU@users.noreply.github.com>
2024-11-01 08:29:57 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# qwerty
+Curiosidades fodas!
+Você mesmo verá cartões com respostas prontas para virar!!!!!!!!!!!!!!!!!!!!!
---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Curiosidade',
+    'Qual a junção de Quero-Quero com Pica-Pau?',
+    'IHHHHHHHHHHHH'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
